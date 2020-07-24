dffe30847a460b106cf90b3a0aae5dc2a45702d7
Muhammad Mufeez Ahmed <muhammad.ahmed@valuelabs.com>
2020-07-24 16:47:48 +0530

    adding custom command

--- support/commands.js
+++ support/commands.js
@@ -11,14 +11,32 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add("login", (email, password) => { ... })
+//import 'cypress-file-upload';
+
+Cypress.Commands.add('upload_file', (fileName, fileType = ' ', selector) => {
+    cy.get(selector).then(subject => {
+      cy.fixture(fileName, 'base64')
+        .then(Cypress.Blob.base64StringToBlob)
+        .then(blob => {
+          const el = subject[0]
+          const testFile = new File([blob], fileName, { type: fileType })
+          const dataTransfer = new DataTransfer()
+          dataTransfer.items.add(testFile)
+          el.files = dataTransfer.files
+        })
+    })
+  })
 
 Cypress.Commands.add("login", (email, password) => {
-    cy.get('#username')
-     .type(email)
+  if (cy.get('#username').should('exist'))
+  {
+  cy.get('#username').type(email)
 
-  cy.get('#password').type(password)
+  cy.get('#password').type(password,{ log: false })
   cy.get('#main > div.container > div:nth-child(1) > div > div > div > form > button').click(); 
+  }
  })
+
  
 //
 //
