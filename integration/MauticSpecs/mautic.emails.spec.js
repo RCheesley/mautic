58cf3131dc9c4120b53c83a73a4f934488d769d7
Muhammad Mufeez Ahmed <muhammad.ahmed@valuelabs.com>
2020-09-18 20:56:39 +0530

    adding delete test

--- integration/MauticSpecs/mautic.emails.spec.js
+++ integration/MauticSpecs/mautic.emails.spec.js
@@ -4,6 +4,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const leftNavigation = require("../../Pages/LeftNavigation");
 const emails = require("../../Pages/Emails");
+const search=require("../../Pages/Search");
 
 context("Emails", () => {
 
@@ -18,6 +19,35 @@ context("Emails", () => {
     emails.emailSubject.type('Test Email by Cypress');
     emails.emailInternalName.type('Test Email by Cypress')
     emails.saveEmailButton.click();
+    emails.closeButton.click({force: true});
+  });
+
+  it("Add new Email for deletion", () => {
+   // leftNavigation.ChannelsSection.click();
+   // leftNavigation.EmailsSubSection.click();
+    cy.wait(3000);
+    emails.addNewButton.click({ force: true });
+    cy.wait(2000);
+    emails.templateEmailSelector.click();
+    cy.wait(2000);
+    emails.emailSubject.type('Delete');
+    emails.emailInternalName.type('Delete')
+    emails.saveEmailButton.click();
+    emails.closeButton.click({force: true});
+  });
+
+  it("Search and Delete Email", () => {
+   // leftNavigation.ChannelsSection.click();
+   // leftNavigation.EmailsSubSection.click();
+    cy.wait(1000);
+    search.searchBox.clear();
+    search.searchBox.type("Delete");
+    search.selectCheckBoxForFirstItem.click({force: true});
+    cy.wait(2000);
+    search.OptionsDropdownForFirstItem.click();
+    search.deleteButtonForFirstItem.click({force: true});
+    search.confirmDeleteButton.click();
+    cy.wait(1000);
   });
 
   });
