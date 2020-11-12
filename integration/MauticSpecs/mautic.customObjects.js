a8efe3c1c6e2fcf5d523945426d04fe222ea3994
Akshay Hukire <akshay.hukire@acquia.com>
2020-11-12 17:58:21 +0530

    Added feature to link custom object to contact

--- integration/MauticSpecs/mautic.customObjects.js
+++ integration/MauticSpecs/mautic.customObjects.js
@@ -6,8 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const settings = require("../../Pages/Settings");
 const search =require("../../Pages/Search");
 const customObject =require("../../Pages/CustomObject");
+const contact = require("../../Pages/Contacts");
+const leftNavigation = require("../../Pages/LeftNavigation");
 
 var carCustomObjectc = "Cars";
+var testContact = "TestContact";
+var instanceCustomObject = "BMW X1";
 context("Create a custom object", () => {
 
   it("Create a new custom object", () => {
@@ -67,7 +71,40 @@ context("Create a custom object", () => {
     customObject.saveAndClose.click();
     customObject.waitforCustomebject();
   });
-  
+
+  it("Add instance to newly added custom object", () => {
+    leftNavigation.customObjectSection.click();
+    customObject.selectCreatedCustomObject.contains(carCustomObjectc).click();
+    customObject.waitForCustomObjectPageLoad();
+    customObject.addNewCustomObjectInstance.click();
+    customObject.waitTillClickedInstanceCustomObjectGetsOpened();
+    customObject.addNewInstanceWithCustomObject.type(instanceCustomObject);
+    customObject.typeNumberForInstanceCreated.type("1111");
+    customObject.typeAndSearchObjectCreated.type('BMW');
+    customObject.selectSearchedPropertyOfCustomObject.click();
+    customObject.saveAndCloseInstanceCreated.click();
+    customObject.waitforCustomebject();
+  });
+
+  it("Link newly added custom object to contact", () => {
+    leftNavigation.contactsSection.click();
+    contact.waitforPageLoad();
+    search.searchBox.clear();
+    search.searchBox.type(testContact);
+    contact.waitTillSearchResultGetsDisplayed();
+    cy.wait(1000);
+    contact.searchAndClickForFirstElement.contains(testContact).click();
+    contact.waitForContactOpen();
+    contact.clickOnCustomObject.contains(carCustomObjectc).click();
+    contact.clickOnLinkExisting.click();
+    contact.waitTillLinkPopupOpen();
+    contact.clickOnDropdwonForLinkObject.click();
+    contact.clickOnLinkObject.click();
+    contact.checkNoResultFoundMessage.should('contain','No Results Found');
+    contact.closeThePopUpWindow.click();
+    contact.customObjectTable.should('contain', instanceCustomObject);
+  });
+
   it("Search and delete newly added custom object", () => {
     settings.settingsMenuButton.click();
     settings.customObjectsSection.click();
