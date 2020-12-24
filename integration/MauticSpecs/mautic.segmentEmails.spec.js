005c8cdc9f3259a5f6605143f3e42fb599d46601
Akshay Hukire <akshay.hukire@acquia.com>
2020-12-24 19:26:43 +0530

    Removed additional waits and corrected locator

--- integration/MauticSpecs/mautic.segmentEmails.spec.js
+++ integration/MauticSpecs/mautic.segmentEmails.spec.js
@@ -4,51 +4,31 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const leftNavigation = require("../../Pages/LeftNavigation");
 const emails = require("../../Pages/Emails");
-const search=require("../../Pages/Search");
-const email = require("../../Pages/Emails");
 
-context("Emails", () => {
+var segmentEmail = "testSegmentEmailCypress";
+
+context("Segment Emails", () => {
 
   it("Add new segment Email", () => {
     leftNavigation.ChannelsSection.click();
     leftNavigation.EmailsSubSection.click({ force: true });
     emails.waitforPageLoad();
     emails.addNewButton.click({ force: true });
-    cy.wait(2000);
     emails.segmentEmailSelector.click();
-    cy.wait(2000);
-    emails.emailSubject.type('testSegmentEmailCypress');
-    emails.emailInternalName.type('testSegmentEmailCypress')
+    emails.emailSubject.type(segmentEmail);
+    emails.emailInternalName.type(segmentEmail)
     emails.contactSegmentSelector.click();
     emails.firstSegmentEmailSelector.click();
     emails.saveEmailButton.click();
     emails.closeButton.click();
     emails.waitforEmailCreation();
-    search.searchBox.clear();
-    search.searchBox.type("testSegmentEmailCypress");
-    cy.wait(2000);
-    emails.searchAndSelectEmail.contains('testSegmentEmailCypress').click();
-    cy.wait(2000);
+    cy.visit('/s/emails?search=' + segmentEmail)
+    emails.searchAndSelectEmail.contains(segmentEmail).click();
+    emails.waitTillCreatedSegmentEmailGetsOpen();
     emails.scheduleSegmentEmail.click();
     emails.scheduleButton.click();
     cy.wait(5000);
   });
-
-  it("Search and Delete newly added segment Email", () => {
-    leftNavigation.ChannelsSection.click();
-    leftNavigation.EmailsSubSection.click();
-    cy.wait(3000);
-    emails.waitforPageLoad();
-    search.searchBox.clear();
-    search.searchBox.type("testSegmentEmailCypress");
-    cy.wait(2000);
-    search.selectCheckBoxForFirstItem.should('be.visible')
-    search.selectCheckBoxForFirstItem.click();
-    search.OptionsDropdownForFirstItem.click();
-    search.deleteButtonForFirstItem.click();
-    search.confirmDeleteButton.click();
-  });
-
   });
 
 
