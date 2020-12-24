4beaa7d9ebe9e79f7f483dd225432053d4e484f6
Akshay Hukire <akshay.hukire@acquia.com>
2020-12-24 19:27:48 +0530

    Removed static wait and corected locators

--- integration/MauticSpecs/mautic.segments.spec.js
+++ integration/MauticSpecs/mautic.segments.spec.js
@@ -4,16 +4,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const leftNavigation = require("../../Pages/LeftNavigation");
 const segments = require("../../Pages/Segments");
-const search = require("../../Pages/Search");
+
+var cypressSegment = "CypressSegment"
 
 context("Segments", () => {
   it("Add new Segment", () => {
     leftNavigation.SegmentsSection.click();
-    cy.wait(1000);
     segments.waitForPageLoad();
     segments.addNewButton.click({ force: true });
     cy.wait(1000);
-    segments.segmentName.type("Cypress");
+    segments.segmentName.type(cypressSegment);
     segments.filterTab.click();
     cy.wait(1000);
     segments.filterDropDown.click();
@@ -28,11 +28,8 @@ context("Segments", () => {
   it("Edit newly added segment", () => {
     leftNavigation.SegmentsSection.click();
     segments.waitForPageLoad();
-    search.searchBox.clear();
-    segments.SearchBox.type("Cypress");
-    segments.waitTillSearchedElementGetsVisible();
-    cy.wait(1000);
-    segments.searchAndSelectSegment.contains("Cypress").click();
+    cy.visit('/s/segments?search=' + cypressSegment)
+    segments.searchAndSelectSegment.contains(cypressSegment).click();
     segments.waitTillClickedSegmentGetsOpen();
     segments.editSegment.click();
     segments.filterTab.click();
@@ -50,10 +47,7 @@ context("Segments", () => {
   it("Search and Delete Segment", () => {
     leftNavigation.SegmentsSection.click();
     segments.waitForPageLoad();
-    segments.SearchBox.click().clear();
-    segments.SearchBox.type("Cypress");
-    segments.waitTillSearchedElementGetsVisible();
-    cy.wait(1000);
+    cy.visit('/s/segments?search=' + cypressSegment)
     segments.firstCheckbox.click();
     segments.firstDropDown.click();
     segments.deleteOption.click();
