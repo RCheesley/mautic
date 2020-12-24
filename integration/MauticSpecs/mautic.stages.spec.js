5847f8695c68a195e0c86f3030fdbc2f1a3e1364
Akshay Hukire <akshay.hukire@acquia.com>
2020-12-24 19:28:26 +0530

    Corrected wait issue

--- integration/MauticSpecs/mautic.stages.spec.js
+++ integration/MauticSpecs/mautic.stages.spec.js
@@ -6,12 +6,14 @@ const leftNavigation = require("../../Pages/LeftNavigation");
 const stages = require("../../Pages/Stages");
 const search = require("../../Pages/Search");
 
+var testStage = "TestStage"
+
 context("Add new a stage", () => {
    it("Add a Stage", () => {
     leftNavigation.StagesSection.click();
     stages.checkNoResultFoundMessage.should('contain','No Results Found');
     stages.addNewButton.click();
-    stages.stageName.type("TestStage");
+    stages.stageName.type(testStage);
     stages.stageWeight.type("40");
     stages.saveAndCloseButton.click();
     stages.waitforStageCreation();
@@ -19,10 +21,8 @@ context("Add new a stage", () => {
 
   it("Edit newly added Stage", () => {
     leftNavigation.StagesSection.click();
-    search.searchBox.clear();
-    search.searchBox.type("TestStage");
-    cy.wait(2000);
-    stages.searchAndClickOnFirstElement.contains("Test").click();
+    cy.visit('/s/stages?search=' + testStage)
+    stages.searchAndClickOnFirstElement.contains(testStage).click();
     stages.waitforPageLoad;
     stages.stageWeight.clear();
     stages.stageWeight.type("50");
@@ -32,9 +32,7 @@ context("Add new a stage", () => {
 
   it("search and delete newly added stage", () => {
     leftNavigation.StagesSection.click();
-    search.searchBox.clear();
-    search.searchBox.type("TestStage");
-    cy.wait(2000);
+    cy.visit('/s/stages?search=' + testStage)
     stages.searchAndClickOnFirstCheckbox.click();
     search.selectAndClickFirstItemsOption.click();
     search.deleteButtonForFirstItem.click();
