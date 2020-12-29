b40cace2af0c424f3f362449a00d99867b60ff44
Akshay Hukire <akshay.hukire@acquia.com>
2020-12-29 20:39:38 +0530

    added test to check if user received trigger email

--- integration/MauticSpecs/mautic.points.spec.js
+++ integration/MauticSpecs/mautic.points.spec.js
@@ -4,9 +4,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const leftNavigation = require("../../Pages/LeftNavigation");
 const points = require("../../Pages/Points");
+const emails = require("../../Pages/Emails");
+const contact = require("../../Pages/Contacts");
 const search = require("../../Pages/Search");
+
 var testAction = "testAction"
 var testTrigger = "testTrigger"
+var testTriggerEmail = "TestTriggerEmail"
+var testContact = "testcontact";
+var mailtestUrl = 'https://mailtest.mautic.com/api/v1/mailbox/'
+var getMailId;
 
 context("Points", () => {
    it("Add a Action", () => {
@@ -63,9 +70,9 @@ context("Points", () => {
     points.addEventButton.click();
     points.sendEmailEvent.click();
     cy.wait(1000);
-    points.eventName.type("Test Trigger");
+    points.eventName.type(testTrigger);
     points.emailSelector.click();
-    points.firstEmail.contains("Test").click();
+    points.firstEmail.contains(testTriggerEmail).click();
     points.addButton.click();
     cy.wait(1000);
     points.saveAndCloseTriggerButton.click();
@@ -85,6 +92,57 @@ context("Points", () => {
     points.waitforTriggerToBeCreated();
   })
 
+  it("Edit previously added contact to point 50 to test the trigger", () => {
+    leftNavigation.contactsSection.click();
+    contact.waitforPageLoad();
+    cy.visit('/s/contacts?search=' + testContact);
+    contact.searchAndClickForFirstElement.contains(testContact).click();
+    contact.editContact.click();
+    contact.waitForContactEditPageOpen();
+    contact.updateContactPoints.type("50");
+    contact.SaveButton.click();
+    contact.closeButton.click({ force: true });
+    contact.waitForContactCreation();
+    cy.wait(5000)
+  });
+
+  it("Verify that user has received the trigger Email", () => {
+    cy.wait(5000);
+   cy.request({ 
+     method:'GET',
+     url: mailtestUrl + testContact,
+    }).then(function(response){
+      expect(response).to.have.property('status',200)
+      expect(response.body).to.not.be.null
+      expect(response.body[0]).to.have.property('mailbox',testContact)
+      expect(response.body[0]).to.have.property('subject',testTriggerEmail)
+      expect(response.body[0]).to.have.property('seen',false)
+      const body = (response.body[0])
+      getMailId = body['id']
+    })
+  });
+
+   it("Delete the read email", () => {
+    cy.request({
+      method:'DELETE',
+      url: mailtestUrl + testContact + '/' + getMailId,
+     }).then(function(response){
+       expect(response).to.have.property('status',200)
+     });
+   })
+
+   it("Search and delete trigger email", () => {
+    leftNavigation.ChannelsSection.click();
+    leftNavigation.EmailsSubSection.click();
+    emails.waitforPageLoad();
+    cy.visit('/s/emails?search=' + testTriggerEmail)
+    search.selectCheckBoxForFirstItem.click();
+    search.OptionsDropdownForFirstItem.click();
+    search.deleteButtonForFirstItem.click();
+    search.confirmDeleteButton.click();
+    emails.checkNoResultFoundMessage.should('contain','No Results Found');
+  });
+
   it("Delete newly added Trigger", () => {
     leftNavigation.PointsSection.click();
     points.manageTriggerSection.click();
@@ -95,9 +153,9 @@ context("Points", () => {
     points.editOptionsForFirstSelectionForTrigger.click();
     points.deleteOption.click();
     points.confirmWindowDelete.click();
-    cy.wait(1000);
     points.checkNoResultFoundMessage.should('contain','No Results Found');
   })
+
   });
 
 
