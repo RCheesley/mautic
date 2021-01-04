ac23a5cfbed0f67499a6edf0cb61379b8f809cd9
Akshay Hukire <akshay.hukire@acquia.com>
2021-01-04 15:42:55 +0530

    Added coverage as per the MAT suite

--- integration/MauticSpecs/mautic.roles.spec.js
+++ integration/MauticSpecs/mautic.roles.spec.js
@@ -6,7 +6,7 @@ const settings = require("../../Pages/Settings");
 const roles = require("../../Pages/Roles");
 const search=require("../../Pages/Search");
 
-var readRole = "testConatctsReadRule"
+var readRole = "Standard"
 
 context("Roles", () => {
   it("Add new role for reading contact", () => {
@@ -21,21 +21,13 @@ context("Roles", () => {
     roles.apiAccessGrantedCheckBox.click();
     cy.wait(1000);
     roles.contactPermissionTab.click();
-    roles.contacts_Access_ViewOwn.click();
-    roles.contacts_Access_ViewOthers.click();
+    roles.contacts_Access_Full.click();
+    roles.contacts_Segments_ViewOthers.click()
+    roles.contacts_Segments_EditOthers.click()
+    roles.contacts_import_View.click()
+    roles.contacts_import_Edit.click()
     roles.saveAndCloseTab.click();
     roles.waitforRoleCreation();
-  });
-
-  it("Search and Delete newly added role", () => {
-    settings.settingsMenuButton.click();
-    settings.rolesSection.click({force: true});
-    roles.waitForPageLoad();
-    cy.visit('/s/roles?search='+ readRole);
-    search.selectCheckBoxForFirstItem.click();
-    search.OptionsDropdownForFirstItem.click();
-    search.deleteButtonForFirstItem.click();
-    search.confirmDeleteButton.click();
   });
 
   });
