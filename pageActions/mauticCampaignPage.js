fc27641424b76d6043d16461626fbf57ea6b01e9
Akshay Hukire <akshay.hukire@acquia.com>
2021-05-25 13:00:26 +0530

    Added steps for campaign step definition

---
+++ pageActions/mauticCampaignPage.js
@@ -0,0 +1,159 @@
+import { Cutils } from "../CommonUtils/Cutils";
+import { mauticCampaignsPageElements } from "../pageElements/mauticCampaignsPageElements";
+import { mauticGlobalPage } from "./mauticGlobalPage";
+
+export class mauticCampaignPage {
+  static clickAddNewButton() {
+    Cutils.IsVisible(mauticCampaignsPageElements.addNewButton);
+    Cutils.click(mauticCampaignsPageElements.addNewButton);
+  }
+  static typeCampaignTitle(text) {
+    mauticGlobalPage.waitForPageLoad("New Campaign");
+    Cutils.IsVisible(mauticCampaignsPageElements.campaignName);
+    Cutils.typeText(mauticCampaignsPageElements.campaignName, text);
+  }
+  static clickLaunchCampaignBuilder() {
+    Cutils.IsVisible(mauticCampaignsPageElements.campaignBuilder);
+    Cutils.click(mauticCampaignsPageElements.campaignBuilder);
+  }
+  static selectCampaignSource(text) {
+    Cutils.IsVisible(mauticCampaignsPageElements.campaignAddContactSource);
+    Cutils.selectValueFromDropDownNonSelect(
+      mauticCampaignsPageElements.campaignAddContactSource,
+      text
+    );
+  }
+  static selectSegmentSource(text) {
+    mauticGlobalPage.waitForPageLoad("Contact Source");
+    Cutils.IsVisible(mauticCampaignsPageElements.segmentSource);
+    Cutils.selectValueFromDropDownNonSelect(
+      mauticCampaignsPageElements.segmentSource,
+      text
+    );
+    Cutils.click(mauticCampaignsPageElements.addButtonForModel);
+    Cutils.isContains(mauticCampaignsPageElements.checkSegmentIsSelected, text);
+  }
+  static clickCampaignEventList() {
+    Cutils.scrollToView(mauticCampaignsPageElements.campaignEventsList);
+    Cutils.click(mauticCampaignsPageElements.campaignEventsList);
+  }
+  static selectEventToPerform(text) {
+    if (text == "Action") {
+      Cutils.IsVisible(mauticCampaignsPageElements.tabSelectAction);
+      Cutils.click(mauticCampaignsPageElements.tabSelectAction);
+    }
+    if (text == "Condition") {
+      Cutils.IsVisible(mauticCampaignsPageElements.tabSelectCondition);
+      Cutils.click(mauticCampaignsPageElements.tabSelectCondition);
+    }
+    if (text == "Decision") {
+      Cutils.IsVisible(mauticCampaignsPageElements.tabSelectDecision);
+      Cutils.click(mauticCampaignsPageElements.tabSelectDecision);
+    }
+  }
+  static selectActionFromDropDown(text) {
+    Cutils.IsVisible(mauticCampaignsPageElements.searchTextBoxForActions);
+    Cutils.selectValueFromDropDownNonSelect(
+      mauticCampaignsPageElements.searchTextBoxForActions,
+      text
+    );
+  }
+  static addContactInDNC(eventName, channel) {
+    Cutils.IsVisible(mauticCampaignsPageElements.addNameForEvent);
+    Cutils.typeText(mauticCampaignsPageElements.addNameForEvent, eventName);
+    Cutils.selectValueFromDropDownNonSelect(
+      mauticCampaignsPageElements.selectChannelForDNC,
+      channel
+    );
+    Cutils.click(mauticCampaignsPageElements.addButtonForModel);
+  }
+  static applyChangesToBuilder() {
+    Cutils.IsVisible(mauticCampaignsPageElements.applyChangesToBuilder);
+    Cutils.click(mauticCampaignsPageElements.applyChangesToBuilder);
+    Cutils.isEnabled(mauticCampaignsPageElements.applyChangesToBuilder);
+    Cutils.click(mauticCampaignsPageElements.closeBuilder);
+  }
+  static Publish_SaveTheCampaign() {
+    Cutils.IsVisible(mauticCampaignsPageElements.campaignPublished_Yes);
+    Cutils.click(mauticCampaignsPageElements.campaignPublished_Yes);
+    Cutils.click(mauticCampaignsPageElements.campaignSaveAndClose);
+  }
+  static hardCodedWaitAddedforCampaignGetsPublished() {
+    Cutils.waitForTime(10000);
+    Cutils.reloadPage();
+  }
+  static selectCampaignTab(text) {
+    if (text == "Preview") {
+      Cutils.IsVisible(mauticCampaignsPageElements.campaignPreview);
+      Cutils.click(mauticCampaignsPageElements.campaignPreview);
+    }
+    if (text == "Actions") {
+      Cutils.IsVisible(mauticCampaignsPageElements.campaignActions);
+      Cutils.click(mauticCampaignsPageElements.campaignActions);
+    }
+    if (text == "Contacts") {
+      Cutils.IsVisible(mauticCampaignsPageElements.campaignContacts);
+      Cutils.click(mauticCampaignsPageElements.campaignContacts);
+    }
+  }
+
+  static isCampaignExecutedSuccessfully() {
+    Cutils.IsVisible(mauticCampaignsPageElements.campiganSuccessLabel);
+    Cutils.isContains(mauticCampaignsPageElements.campiganSuccessLabel, "100%");
+  }
+
+  static selectAndClickOnFirstContactUnderCampaign() {
+    Cutils.IsVisible(
+      mauticCampaignsPageElements.selectFirstContactUnderCampaignTab
+    );
+    Cutils.click(
+      mauticCampaignsPageElements.selectFirstContactUnderCampaignTab
+    );
+  }
+
+  static checkThatConatctHasDoNotContactLabel(label) {
+    if (label == "have") {
+      Cutils.IsVisible(mauticCampaignsPageElements.doNotContactLabel);
+      Cutils.isContains(
+        mauticCampaignsPageElements.doNotContactLabel,
+        "Do Not Contact"
+      );
+    }
+    if (label == "does not have") {
+      Cutils.IsVisible(mauticCampaignsPageElements.doNotContactLabel);
+      Cutils.isNotContains(
+        mauticCampaignsPageElements.doNotContactLabel,
+        "Do Not Contact"
+      );
+    }
+  }
+
+  static selectIncludeEventsBySource(text) {
+    Cutils.clear(mauticCampaignsPageElements.includeEventsBySource);
+    Cutils.IsVisible(mauticCampaignsPageElements.includeEventsBySource);
+    Cutils.selectValueFromDropDownNonSelect(
+      mauticCampaignsPageElements.includeEventsBySource,
+      text
+    );
+  }
+
+  static checkEventsTriggeredSuccessfullyContactHistory(
+    eventName,
+    eventType,
+    eventTimestamp
+  ) {
+    Cutils.IsVisible(mauticCampaignsPageElements.eventNameTriggeredByContact);
+    Cutils.isContains(
+      mauticCampaignsPageElements.eventNameTriggeredByContact,
+      eventName
+    );
+    Cutils.isContains(
+      mauticCampaignsPageElements.eventTypeTriggeredByContact,
+      eventType
+    );
+    Cutils.isContains(
+      mauticCampaignsPageElements.eventTimeStampTriggeredByContact,
+      eventTimestamp
+    );
+  }
+}
