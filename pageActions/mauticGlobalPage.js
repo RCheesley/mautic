f9ac3d242dde101bb9f349344f3f6fc8e7bddc3e
Akshay Hukire <akshay.hukire@acquia.com>
2021-05-25 13:00:52 +0530

    Added visit condition for campaign visit

--- pageActions/mauticGlobalPage.js
+++ pageActions/mauticGlobalPage.js
@@ -1,41 +1,43 @@
 import { Cutils } from "../CommonUtils/Cutils";
 
 export class mauticGlobalPage {
-
-    static goToURL(text) {
-        var input_url;
-        switch (text) {
-            case "Contacts":
-                input_url = 's/contacts';
-                break;
-            case "Emails":
-                input_url = 's/emails';
-                break;
-            case "Segments":
-                input_url = 's/segments';
-                break;
-            case "Asset":
-                input_url = 's/assets';
-                break;
-            case 'Local Asset':
-                input_url = '/s/assets?search=Local';
-                break;
-            case 'Remote Asset':
-                input_url = '/s/assets?search=remote';
-                break;
-            case 'Companies':
-                input_url = 's/companies';
-                break;
-        }
-        console.log(input_url);
-        Cutils.navigateTo(input_url);
+  static goToURL(text) {
+    var input_url;
+    switch (text) {
+      case "Contacts":
+        input_url = "s/contacts";
+        break;
+      case "Emails":
+        input_url = "s/emails";
+        break;
+      case "Segments":
+        input_url = "s/segments";
+        break;
+      case "Asset":
+        input_url = "s/assets";
+        break;
+      case "Local Asset":
+        input_url = "/s/assets?search=Local";
+        break;
+      case "Remote Asset":
+        input_url = "/s/assets?search=remote";
+        break;
+      case "Companies":
+        input_url = "s/companies";
+        break;
+      case "Campaigns":
+        input_url = "s/campaigns";
+        break;
     }
+    console.log(input_url);
+    Cutils.navigateTo(input_url);
+  }
 
-    static verifyPageTitle(text) {
-        return Cutils.isOnPage(text);
-    }
+  static verifyPageTitle(text) {
+    return Cutils.isOnPage(text);
+  }
 
-    static waitForPageLoad(text) {
-        return Cutils.isContains('//h3', text);
-    }
+  static waitForPageLoad(text) {
+    return Cutils.isContains("//h3", text);
+  }
 }
