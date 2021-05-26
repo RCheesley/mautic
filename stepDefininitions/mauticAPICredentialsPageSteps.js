84b5ae7dec640950bc1ec85a2d52e73e0e637b0e
waytocypress <rohit.sakhawalkar@acquia.com>
2021-05-26 19:42:42 +0530

    Verify that user is able to create credentials and update the contact fields using them
    added feature Verify that user is able to create credentials and update the contact fields using them

--- stepDefininitions/mauticAPICredentialsPageSteps.js
+++ stepDefininitions/mauticAPICredentialsPageSteps.js
@@ -1,6 +1,7 @@
 
 import { When, Then } from "cypress-cucumber-preprocessor/steps";
 import { mauticAPICredentialsPage } from "../pageActions/mauticAPICredentialsPage";
+import { when } from "cypress/types/jquery";
 
 
 When(/^I click Add New Button on API Credentials Page$/, () => {
@@ -43,9 +44,64 @@ Then(/^I should see "([^"]*)" on API Credentials Page$/, (text) => {
     return mauticAPICredentialsPage.areCredentialsDeleted(text);
 });
 
-
 When(/^I select Authentication Type as "([^"]*)" on API Credentials Page$/, (text) => {
     mauticAPICredentialsPage.selectAuthenticationTypeAs(text);
 });
+
+When(/^I get Credentials$/, () => {
+    mauticAPICredentialsPage.getCredentials();
+});
+
+
+When(/^I make Auth API Call$/, () => {
+    mauticAPICredentialsPage.makeAuthAPICall();
+});
+
+Then(/^I should see bearer token is generated$/, () => {
+    return mauticAPICredentialsPage.isBearerTokenGenerated();
+});
+
+When(/^I make Auth API Call with Invalid Credentials$/, () => {
+    mauticAPICredentialsPage.makeAuthAPICallWithInvalidCredentials();
+});
+
+Then(/^should error response$/, () => {
+    return mauticAPICredentialsPage.isError();
+});
+
+When(/^I make API call to create contact$/, () => {
+    mauticAPICredentialsPage.makeAPIcallToCreateContact();
+});
+
+Then(/^I should see Country field is empty$/, () => {
+    return mauticAPICredentialsPage.isCountryFieldEmpty();
+});
+
+When(/^I update contact Country field with value "([^"]*)"$/, (text) => {
+    mauticAPICredentialsPage.updateContactCountryFieldWithValue(text);
+});
+
+Then(/^I should see Country field value is updated with value "([^"]*)"$/, (text) => {
+    return mauticAPICredentialsPage.isUpdatedWithValue(text);
+});
+
+When(/^I make API call to create contact with First Name as "([^"]*)" Last name as "([^"]*)" emailID as "([^"]*)"$/, (text1, text2, text3) => {
+    mauticAPICredentialsPage.makeAPIcallToCreateContactWithDetails(text1, text2, text3);
+});
+
+Then(/^I should see Contact is Created using API$/, () => {
+    return mauticAPICredentialsPage.isContactCreatedusingAPI();
+});
+
+When(/^I delete Last Created contact using API$/, () => {
+    mauticAPICredentialsPage.deleteContact();
+});
+
+Then(/^I should see Contact is deleted using API$/, () => {
+    return mauticAPICredentialsPage.isContactDeleted();
+});
+
+
+
 
 
