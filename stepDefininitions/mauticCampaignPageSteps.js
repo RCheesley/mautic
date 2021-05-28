70604f11acb4caec051513890d71600db2d9a16a
Akshay Hukire <akshay.hukire@acquia.com>
2021-05-28 17:26:47 +0530

    Updated step Definition_added new as feature added

--- stepDefininitions/mauticCampaignPageSteps.js
+++ stepDefininitions/mauticCampaignPageSteps.js
@@ -48,6 +48,10 @@ And(
   }
 );
 
+When(/^I add event name "([^"]*)" and add stage "([^"]*)"$/, (args1, args2) => {
+  mauticCampaignPage.changeContactStage(args1, args2);
+});
+
 And(
   /^I add event name "([^"]*)" and add to the company score "([^"]*)"$/,
   (args1, args2) => {
@@ -55,32 +59,35 @@ And(
   }
 );
 
-Then(
-  /^I click on "([^"]*)" logo and I check that Company’s score is "([^"]*)"$/,
-  (args1, args2) => {
-    mauticCampaignPage.clickOnCompanyNameAndCheckScore(args1, args2);
-  }
-);
-
 And(/^I apply changes to builder and close the builder$/, () => {
   mauticCampaignPage.applyChangesToBuilder();
+});
+
+Then(/^I check company "([^"]*)" score is "([^"]*)"$/, (args1, args2) => {
+  mauticCampaignPage.checkCompanyAndScore(args1, args2);
 });
 
 Then(/^I publish the campaign and save it$/, () => {
   mauticCampaignPage.Publish_SaveTheCampaign();
 });
 
-And(/^I wait for 10 sec to apply campaign action on segments contacts$/, () => {
-  mauticCampaignPage.hardCodedWaitAddedforCampaignGetsPublished();
-});
+When(
+  /^I wait for 12 sec to apply campaign action on segments contacts$/,
+  () => {
+    mauticCampaignPage.hardCodedWaitAddedforCampaignGetsPublished();
+  }
+);
 
 When(/^I click on "([^"]*)" tab$/, (args1) => {
   mauticCampaignPage.selectCampaignTab(args1);
 });
 
-And(/^verify that campaign is executed successfully$/, () => {
-  mauticCampaignPage.isCampaignExecutedSuccessfully();
-});
+Then(
+  /^verify that campaign is executed successfully with "([^"]*)" action completion$/,
+  (args1) => {
+    mauticCampaignPage.isCampaignExecutedSuccessfully(args1);
+  }
+);
 
 Then(
   /^I check that campaign triggered successfully with "([^"]*)" and event "([^"]*)"$/,
@@ -126,6 +133,14 @@ Then(
   }
 );
 
+Then(
+  /^I check that second event triggered successfully Event Name "([^"]*)" and Event Type "([^"]*)" and Event Timestamp "([^"]*)"$/,
+  (args1, args2, args3) => {
+    console.log(args1, args2, args3);
+    return true;
+  }
+);
+
 Then(/^I check that contact has gained "([^"]*)"$/, (args1) => {
   mauticCampaignPage.checkContactGainPoints(args1);
 });
@@ -140,4 +155,34 @@ When(/^I delete campaign "([^"]*)"$/, (args1) => {
 
 Then(/^I should see campaign "([^"]*)" is deleted$/, (args1) => {
   mauticCampaignPage.isCampaignDeleted(args1);
+});
+
+Then(/^I check that "([^"]*)" present in contact details Page$/, (args1) => {
+  mauticCampaignPage.checkStage(args1);
+});
+
+When(
+  /^I add event name "([^"]*)" and choose segment in which i want to add contact "([^"]*)" and remove contact's from segment "([^"]*)"$/,
+  (args1, args2, args3) => {
+    mauticCampaignPage.addAndRemoveSegment(args1, args2, args3);
+  }
+);
+
+Then(/^I check that "([^"]*)" is present under contacts tab$/, (args1) => {
+  mauticCampaignPage.noContactsPresent(args1);
+});
+
+Then(
+  /^I check that "([^"]*)" segment contain "([^"]*)" and I click on it$/,
+  (args1, args2) => {
+    mauticCampaignPage.checkSegmentContainesExactContacts(args1, args2);
+  }
+);
+
+When(/^I click on first contact from that segment$/, () => {
+  mauticCampaignPage.clickOnFirstContactUnderSegment();
+});
+
+Then(/^I check that it has no contacts "([^"]*)"$/, (args1) => {
+  mauticCampaignPage.noContactsPresent(args1);
 });
