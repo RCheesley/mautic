43d395017bfdcb4d3e0f901a823bb77363dbecf0
Akshay Hukire <akshay.hukire@acquia.com>
2021-06-08 13:46:19 +0530

    added stepdefinition for campaign regression sheet

--- pageActions/mauticCampaignPage.js
+++ pageActions/mauticCampaignPage.js
@@ -1,7 +1,9 @@
 import { Cutils } from "../CommonUtils/Cutils";
 import { mauticCampaignsPageElements } from "../pageElements/mauticCampaignsPageElements";
 import { mauticGlobalPage } from "./mauticGlobalPage";
-
+import { mauticCampaignsPageElements } from "cypress/pageElements/mauticCampaignsPageElements";
+const emailId;
+var gifLinkInEmail;
 export class mauticCampaignPage {
   static clickAddNewButton() {
     Cutils.IsVisible(mauticCampaignsPageElements.addNewButton);
@@ -36,6 +38,10 @@ export class mauticCampaignPage {
   static clickCampaignEventList() {
     Cutils.scrollToView(mauticCampaignsPageElements.campaignEventsList);
     Cutils.click(mauticCampaignsPageElements.campaignEventsList);
+  }
+  static clickCampaignEventAfterAction() {
+    Cutils.scrollToView(mauticCampaignsPageElements.campaignEventAfterAction);
+    Cutils.click(mauticCampaignsPageElements.campaignEventAfterAction);
   }
   static selectEventToPerform(text) {
     Cutils.click(
@@ -86,10 +92,37 @@ export class mauticCampaignPage {
     Cutils.click(mauticCampaignsPageElements.addButtonForModel);
   }
 
+    static updateCompanyFields(eventName, city, country, noOfEmployee) {
+    Cutils.IsVisible(mauticCampaignsPageElements.addNameForEvent);
+    Cutils.typeText(mauticCampaignsPageElements.addNameForEvent, eventName);
+    Cutils.typeText(mauticCampaignsPageElements.UpdateCompanyFieldCity, city);
+    Cutils.selectValueFromDropDownNonSelect(
+      mauticCampaignsPageElements.updateCompanyFieldsCountry,
+      country
+    );
+    Cutils.typeText(mauticCampaignsPageElements.updateCompanyFieldNumberOfEmployee, noOfEmployee)
+    Cutils.click(mauticCampaignsPageElements.addButtonForModel);
+  }
+
   static addToCompanyScore(eventName, score) {
     Cutils.IsVisible(mauticCampaignsPageElements.addNameForEvent);
     Cutils.typeText(mauticCampaignsPageElements.addNameForEvent, eventName);
     Cutils.typeText(mauticCampaignsPageElements.companyScore, score);
+    Cutils.click(mauticCampaignsPageElements.addButtonForModel);
+  }
+
+  static sendEmail(eventName, emailName) {
+    Cutils.IsVisible(mauticCampaignsPageElements.addNameForEvent);
+    Cutils.typeText(mauticCampaignsPageElements.addNameForEvent, eventName);
+    Cutils.selectValueFromDropDownNonSelect(
+      mauticCampaignsPageElements.emailToSend,
+      emailName
+    );
+    Cutils.click(mauticCampaignsPageElements.addButtonForModel);
+  }
+  static ifOpensAnEmail(eventName) {
+    Cutils.IsVisible(mauticCampaignsPageElements.addNameForEvent);
+    Cutils.typeText(mauticCampaignsPageElements.addNameForEvent, eventName);
     Cutils.click(mauticCampaignsPageElements.addButtonForModel);
   }
   static addAndRemoveSegment(
@@ -155,6 +188,7 @@ export class mauticCampaignPage {
     Cutils.click(
       mauticCampaignsPageElements.selectFirstContactUnderCampaignTab
     );
+    Cutils.IsVisible(mauticCampaignsPageElements.contactsEngagement);
   }
 
   static checkThatConatctHasDoNotContactLabel(label) {
@@ -176,6 +210,7 @@ export class mauticCampaignPage {
   }
 
   static selectIncludeEventsBySource(text) {
+    Cutils.pageReload();
     Cutils.clear(mauticCampaignsPageElements.includeEventsBySource);
     Cutils.IsVisible(mauticCampaignsPageElements.includeEventsBySource);
     Cutils.selectValueFromDropDownNonSelect(
@@ -183,6 +218,7 @@ export class mauticCampaignPage {
       text
     );
     Cutils.IsNotExist(mauticCampaignsPageElements.loadingBarActive);
+    Cutils.waitForTime(1000);
   }
 
   static checkEventsTriggeredSuccessfullyContactHistory(
@@ -230,6 +266,18 @@ export class mauticCampaignPage {
     Cutils.IsVisible(mauticCampaignsPageElements.contactsPoints);
     Cutils.isContains(mauticCampaignsPageElements.contactsPoints, points);
   }
+    static clickOnCompanyName(companyName) {
+    Cutils.IsVisible('//div/a[contains(text(),"' + companyName + '")]');
+    Cutils.click('//div/a[contains(text(),"' + companyName + '")]');
+    }
+  
+  static checkCompanyDetails(city, country, noOfEmployee) {
+    Cutils.IsVisible(mauticCampaignsPageElements.companyDetailsPageCity)
+    Cutils.inputFieldContainsValue(mauticCampaignsPageElements.companyDetailsPageCity, city)
+    Cutils.dropDownFieldSelectedValue(mauticCampaignsPageElements.companyDetailsPageCountry, country)
+    Cutils.click(mauticCampaignsPageElements.companyDetailsProfessionalPage);
+    Cutils.inputFieldContainsValue(mauticCampaignsPageElements.updateCompanyFieldNumberOfEmployee, noOfEmployee);
+  }
 
   static checkCompanyIsPresentInContactsGrid(company) {
     Cutils.isContains(
@@ -238,16 +286,37 @@ export class mauticCampaignPage {
     );
   }
 
+  static selectConditionOption(condition) {
+    Cutils.IsVisible(
+      '//div[contains(@class,"jtk-endpoint jtk-endpoint-anchor-' +
+        condition +
+        '")]'
+    );
+    Cutils.click(
+      '//div[contains(@class,"jtk-endpoint jtk-endpoint-anchor-' +
+        condition +
+        '")]'
+    );
+  }
+
   static checkCompanyIsPresentInContactDetailsPage(company) {
     Cutils.isContains(
       mauticCampaignsPageElements.companiesInContactDetails,
       company
     );
+    Cutils.IsVisible(mauticCampaignsPageElements.primaryCompanyCheck);
   }
 
-  static searchCampaign(campaignName) {
+  static search(text) {
     Cutils.IsVisible(mauticCampaignsPageElements.filter);
-    Cutils.typeText(mauticCampaignsPageElements.filter, campaignName);
+    Cutils.typeText(mauticCampaignsPageElements.filter, text)
+    Cutils.IsVisible('//div[contains(text(),"' + text + '")]');
+  }
+
+  static searchCampaign(text) {
+    Cutils.IsVisible(mauticCampaignsPageElements.filter);
+    Cutils.typeText(mauticCampaignsPageElements.filter, text)
+    Cutils.IsVisible('//a[contains(text(),"' + text + '")]');
   }
   static deleteCampaign(text) {
     Cutils.click(
@@ -279,6 +348,38 @@ export class mauticCampaignPage {
   ) {
     Cutils.isContains(mauticCampaignsPageElements.actionsEventsName, eventName);
     Cutils.isContains(mauticCampaignsPageElements.actionPerformed, action);
+  }
+
+  static checkThatEventIsTriggeredAndAddedCorrectlyInActions_FirstTab(
+    eventName,
+    action,
+    successPercentage
+  ) {
+    Cutils.isContains(mauticCampaignsPageElements.firstEventName, eventName);
+    Cutils.isContains(
+      mauticCampaignsPageElements.actionPerformedFirstTab,
+      action
+    );
+    Cutils.isContains(
+      mauticCampaignsPageElements.successPercentageOfFirstEvent,
+      successPercentage
+    );
+  }
+
+  static checkThatEventIsTriggeredAndAddedCorrectlyInActions_SecondTab(
+    eventName,
+    action,
+    successPercentage
+  ) {
+    Cutils.isContains(mauticCampaignsPageElements.secondEventName, eventName);
+    Cutils.isContains(
+      mauticCampaignsPageElements.actionPerformedSecondTab,
+      action
+    );
+    Cutils.isContains(
+      mauticCampaignsPageElements.successPercentageOfSecondEvent,
+      successPercentage
+    );
   }
 
   static checkCompanyAndScore(companyName, score) {
@@ -319,7 +420,163 @@ export class mauticCampaignPage {
     Cutils.IsVisible(mauticCampaignsPageElements.contactsEngagement);
   }
 
+  static checkContactsPoints(contactName, points) {
+    Cutils.IsVisible(
+      '//div[contains(text(),"' + contactName + '")]//following::td/span'
+    );
+    Cutils.isContains(
+      '//div[contains(text(),"' + contactName + '")]//following::td/span',
+      points
+    );
+  }
+
+  static selectDecisionFromDropDown(decisionText) {
+    Cutils.IsVisible(mauticCampaignsPageElements.searchTextBoxForDecision);
+    Cutils.selectValueFromDropDownNonSelect(
+      mauticCampaignsPageElements.searchTextBoxForDecision,
+      decisionText
+    );
+  }
+
   static noResultsFound(text) {
     return Cutils.isContains('//div[@class="page-list"]//h4', text);
+  }
+
+  static noResultsFound(text) {
+    return Cutils.isContains('//div[@class="page-list"]//h4', text);
+  }
+
+  static checkMailboxOfContact(contactName, emailName) {
+    cy.request({
+      method: "GET",
+      url: Cypress.env("mailtestUrl") + contactName,
+    }).then(function (response) {
+      expect(response).to.have.property("status", 200);
+      expect(response.body).to.not.be.null;
+      expect(response.body[0]).to.have.property("mailbox", contactName);
+      expect(response.body[0]).to.have.property("subject", emailName);
+      expect(response.body[0]).to.have.property("seen", false);
+      let body = response.body[0];
+      emailId = body["id"];
+    });
+  }
+
+  static getMailId(contactName) {
+    var readEmailGifLink;
+    cy.request({
+      method: "GET",
+      url: Cypress.env("mailtestUrl") + contactName + "/" + emailId,
+    }).then(function (response) {
+      var EmailText = JSON.stringify(response);
+      var patternForEmailImages = new RegExp("<img[^>]+>");
+      var imageElements = patternForEmailImages.exec(EmailText);
+      var patternForImageSources = new RegExp('src=\\\\"(.*?)"');
+      readEmailGifLink = patternForImageSources.exec(
+        imageElements[0].toString()
+      );
+      readEmailGifLink = readEmailGifLink[0]
+        .toString()
+        .substring(6)
+        .replace(/\"/, "")
+        .replace("\\", "");
+       gifLinkInEmail = readEmailGifLink;
+      expect(response).to.have.property('status',200)
+      expect(response.body).to.not.be.null
+      mauticCampaignPage.readEmail();
+      mauticCampaignPage.deleteEmail(contactName);
+    });
+  }
+  static readEmail() {
+    cy.request({
+      method: "GET",
+      url: gifLinkInEmail,
+    }).then(function (response) {
+      expect(response).to.have.property("status", 200);
+    });
+  }
+  static deleteEmail(contactName) {
+    cy.request({
+      method: "DELETE",
+      url: Cypress.env("mailtestUrl") + contactName + "/" + emailId,
+    }).then(function (response) {
+      expect(response).to.have.property("status", 200);
+    });
+  }
+
+  static typeDateFieldValuesInContacts(dateField1, dateField2, dateField3) {
+    Cutils.typeText(
+      mauticCampaignsPageElements.updateContact_dateField1,
+      dateField1
+    );
+    Cutils.typeText(
+      mauticCampaignsPageElements.updateContact_dateField2,
+      dateField2
+    );
+    Cutils.typeText(
+      mauticCampaignsPageElements.updateContact_dateField3,
+      dateField3
+    );
+  }
+
+  static updateContactDateFieldValues(
+    eventName,
+    absoluteDate,
+    additionRelativeDate,
+    subtractelativeDate
+  ) {
+    Cutils.IsVisible(mauticCampaignsPageElements.addNameForEvent);
+    Cutils.typeText(mauticCampaignsPageElements.addNameForEvent, eventName);
+    Cutils.typeText(
+      mauticCampaignsPageElements.updateContact_dateField1,
+      absoluteDate
+    );
+    Cutils.scrollToView(mauticCampaignsPageElements.updateContact_dateField2);
+    Cutils.typeText(
+      mauticCampaignsPageElements.updateContact_dateField2,
+      additionRelativeDate
+    );
+    Cutils.scrollToView(mauticCampaignsPageElements.updateContact_dateField3);
+    Cutils.typeText(
+      mauticCampaignsPageElements.updateContact_dateField3,
+      subtractelativeDate
+    );
+    Cutils.scrollToView(mauticCampaignsPageElements.addButtonForModel);
+    Cutils.click(mauticCampaignsPageElements.addButtonForModel);
+  }
+  static verifyDateFieldOfContact(dateField1, dateField2, dateField3) {
+    var myCurrentDate = new Date();
+    var updatedRelativeDate = new Date(myCurrentDate);
+    Cutils.click(mauticCampaignsPageElements.contactDetails);
+    Cutils.scrollToView(mauticCampaignsPageElements.contactDetailsDateField1);
+    Cutils.isContains(
+      mauticCampaignsPageElements.contactDetailsDateField1,
+      dateField1
+    );
+    Cutils.isContains(
+      mauticCampaignsPageElements.contactDetailsDateField2,
+      Cutils.formatDate(
+        updatedRelativeDate.setDate(
+          updatedRelativeDate.getDate() + parseInt(dateField2)
+        )
+      )
+    );
+    updatedRelativeDate = myCurrentDate;
+    Cutils.isContains(
+      mauticCampaignsPageElements.contactDetailsDateField3,
+      Cutils.formatDate(
+        updatedRelativeDate.setDate(
+          updatedRelativeDate.getDate() - parseInt(dateField3)
+        )
+      )
+    );
+    Cutils.isContains(
+      mauticCampaignsPageElements.contactDetailsLastDateActive,
+      " "
+    );
+    Cutils.scrollToView(mauticCampaignsPageElements.contactDetailsDateField4);
+    Cutils.isContains(
+      mauticCampaignsPageElements.contactDetailsDateField4,
+      " "
+    );
   }
 }
