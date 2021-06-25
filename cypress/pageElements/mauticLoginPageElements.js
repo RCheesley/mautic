c143145a52cf8f901a34180329de2b3a13af8990
Ruth Cheesley <ruth.cheesley@acquia.com>
2021-06-25 15:25:14 +0100

    Fix for login page element

--- cypress/pageElements/mauticLoginPageElements.js
+++ cypress/pageElements/mauticLoginPageElements.js
@@ -2,5 +2,5 @@
 export class mauticLoginPageElements {
     static username_xpath = '//input[@id="username"]';
     static password_xpath = '//input[@id="password"]';
-    static login_Button_xpath = '//button[text()="Sign In"]';
+    static login_Button_xpath = '//button[text()="login"]'; // Community Specific
 }
